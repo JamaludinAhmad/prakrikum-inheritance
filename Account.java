1360ec68860f7ac84d7fe5f054734f665c7acd6b
Rufus8702 <Jamaludinahmad013@gmail.com>
2022-10-25 14:08:49 +0700

    p

--- Account.java
+++ Account.java
@@ -1,8 +1,8 @@
 public class Account {
     protected double balance;
 
-    public Account(double initLabnce){
-        balance = initLabnce;
+    public Account(double initBalance){
+        balance = initBalance;
     }
 
     public double getBalance(){
@@ -18,6 +18,54 @@ public class Account {
         balance -= amt;
         return true;
     }
+}
 
+class SavingAccount extends Account{
+    private double interestRate;
 
+    public SavingAccount(double initBalance, double balance, double interest_rate){
+        super(initBalance);
+        super.balance = balance;
+        this.interestRate = interest_rate;
+    }
+
+}
+
+class CheckingAccount extends Account{
+    private double overdraftProtection;
+
+    public CheckingAccount(double initBalance, double balance){
+        super(initBalance);
+        this.balance = balance;
+        overdraftProtection = -0.1d;
+
+    }
+
+    public CheckingAccount(double initBalance, double balance, double protect){
+        super(initBalance);
+        super.balance = balance;
+        overdraftProtection = protect;
+    }
+    
+    public boolean withdraw(double amt){
+        super.withdraw(amt);
+        double overdraftNeeded = amt - this.balance;
+        if(balance - amt >= 0){
+            this.balance -= amt;
+            return true;
+        }
+
+        else {
+            if(overdraftProtection == -1.0 || overdraftProtection < overdraftNeeded){
+                return false;
+            }
+
+            else{
+                this.balance = 0.0;
+                overdraftProtection -= overdraftNeeded;
+                return true;
+            }
+        }
+    }
+    
 }
